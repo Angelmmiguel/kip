6088f8e5b481db348c031e9d146729c7f942d810
Angel M de Miguel <angelmm@redborder.net>
2016-04-12 20:34:18 +0200

    Add deploy to heroku button and fix puma config

--- config/puma.rb
+++ config/puma.rb
@@ -1,3 +1,11 @@
 #!/usr/bin/env puma
 
+# Bind port
 bind "tcp://0.0.0.0:#{ENV['RAILS_PORT'] || 3000}"
+
+# Daemonize the server
+environment 'production'
+pidfile 'tmp/pids/puma.pid'
+
+# Correct environment
+environment 'production'
