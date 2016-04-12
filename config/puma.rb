a313308240a84ff977b7b47fba314c059be1bfd6
Angel M de Miguel <angelmm@redborder.net>
2016-04-12 20:59:38 +0200

    Update environment variable to set the port of the app

--- config/puma.rb
+++ config/puma.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env puma
 
 # Bind port
-bind "tcp://0.0.0.0:#{ENV['RAILS_PORT'] || 3000}"
+bind "tcp://0.0.0.0:#{ENV['PORT'] || 3000}"
 
 # Correct environment
 environment ENV['RACK_ENV'] || 'development'
