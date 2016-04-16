d518559a73593acde25a22d7e740cecaf9354d1b
Angel M de Miguel <angelmm@redborder.net>
2016-04-16 10:46:29 +0200

    #38 Include footer in show and modify the text

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 RSpec.describe ArticlesController, type: :controller do
   describe 'Index' do
+    render_views
+
     it 'assigns @articles' do
       create(:article)
       get :index
@@ -20,9 +22,50 @@ RSpec.describe ArticlesController, type: :controller do
     end
 
     it 'renders the index template with success' do
+      create(:article)
       get :index
       expect(response.status).to eq(200)
       expect(response).to render_template('index')
+    end
+
+    it 'renders welcome when no article' do
+      get :index
+      expect(response.status).to eq(200)
+      expect(response.body).to include('Welcome to')
+    end
+  end
+
+  describe 'Show' do
+    it 'an article' do
+      article = create(:article)
+      get :show, id: article.id
+      # Article and text must be initialized
+      expect(assigns(:article)).not_to be_nil
+      expect(assigns(:text)).not_to be_nil
+
+      expect(response.status).to eq(200)
+      expect(response).to render_template('show')
+    end
+  end
+
+  describe 'Footer' do
+    render_views
+
+    it "musn't be showed in welcome" do
+      get :index
+      expect(response.body).not_to include('<footer')
+    end
+
+    it 'must be showed in index' do
+      create(:article)
+      get :index
+      expect(response.body).to include('<footer')
+    end
+
+    it 'must be showed in show' do
+      article = create(:article)
+      get :show, id: article.id
+      expect(response.body).to include('<footer')
     end
   end
 end
