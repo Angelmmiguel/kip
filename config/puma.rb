4e786bcabbd64df446df43edc30288a4eab86da8
Angel M de Miguel <angelmm@redborder.net>
2016-04-17 15:38:29 +0200

    Add a puma config for manual installations

---
+++ config/puma.rb
@@ -0,0 +1,32 @@
+#!/usr/bin/env puma
+
+# Min and Max threads per worker
+threads 1, 6
+
+# Bind port
+bind "tcp://0.0.0.0:#{ENV['PORT'] || 3000}"
+
+# Correct environment
+environment ENV['RACK_ENV'] || 'development'
+
+# Daemonize
+daemonize true
+
+app_dir = File.expand_path('../..', __FILE__)
+tmp_dir = "#{app_dir}/tmp"
+log_dir = "#{app_dir}/log"
+
+# Set up socket location
+bind "unix://#{tmp_dir}/sockets/puma.sock"
+
+# Logging
+stdout_redirect(
+  "#{log_dir}/log/puma.stdout.log",
+  "#{log_dir}/log/puma.stderr.log",
+  true
+)
+
+# Set master PID and state locations
+pidfile "#{tmp_dir}/pids/puma.pid"
+state_path "#{tmp_dir}/pids/puma.state"
+activate_control_app
