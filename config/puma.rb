ca11416d0e3a6be44e6457269d3efaf050b0667b
Angel M de Miguel <angelmm@redborder.net>
2016-04-19 23:18:28 +0200

    Set certain config of puma only for production

--- config/puma.rb
+++ config/puma.rb
@@ -7,26 +7,30 @@ threads 1, 6
 bind "tcp://0.0.0.0:#{ENV['PORT'] || 3000}"
 
 # Correct environment
-environment ENV['RACK_ENV'] || 'production'
+current_env = ENV['RACK_ENV'] || ENV['RAILS_ENV'] || 'development'
+environment current_env
 
-# Daemonize
-daemonize true
+# Production config
+if current_env == 'production'
+  # Daemonize
+  daemonize true
 
-app_dir = File.expand_path('../..', __FILE__)
-tmp_dir = "#{app_dir}/tmp"
-log_dir = "#{app_dir}/log"
+  app_dir = File.expand_path('../..', __FILE__)
+  tmp_dir = "#{app_dir}/tmp"
+  log_dir = "#{app_dir}/log"
 
-# Set up socket location
-bind "unix://#{tmp_dir}/sockets/puma.sock"
+  # Set up socket location
+  bind "unix://#{tmp_dir}/sockets/puma.sock"
 
-# Logging
-stdout_redirect(
-  "#{log_dir}/puma.stdout.log",
-  "#{log_dir}/puma.stderr.log",
-  true
-)
+  # Logging
+  stdout_redirect(
+    "#{log_dir}/puma.stdout.log",
+    "#{log_dir}/puma.stderr.log",
+    true
+  )
 
-# Set master PID and state locations
-pidfile "#{tmp_dir}/pids/puma.pid"
-state_path "#{tmp_dir}/pids/puma.state"
-activate_control_app
+  # Set master PID and state locations
+  pidfile "#{tmp_dir}/pids/puma.pid"
+  state_path "#{tmp_dir}/pids/puma.state"
+  activate_control_app
+end
